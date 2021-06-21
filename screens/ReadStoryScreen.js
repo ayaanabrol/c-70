cefe72442694c8ef9d032c6883a6b7bf1d8c62e0
ayaanabrol <69078639+ayaanabrol@users.noreply.github.com>
2021-06-21 18:49:07 +0530

    Add files via upload

---
+++ screens/ReadStoryScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class ReadStoryScreen extends React.Component {
+    render() {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+          <Text>Read book screen</Text>
+
+        </View>
+      );
+    }
+  }
